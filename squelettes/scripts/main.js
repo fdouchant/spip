2a17690963b302b5b4144a5662da50a585456223
Fabrice Douchant <fabrice.douchant@gmail.com>
2018-04-22 14:12:58 +0100

    add quotes as carousel

--- squelettes/scripts/main.js
+++ squelettes/scripts/main.js
@@ -1,5 +1,11 @@
 jQuery(function ($) {
 
+    // set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 6000,
+    });
+
     // activate textillate
     $('.tlt').textillate();
 
