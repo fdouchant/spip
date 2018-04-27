17571e54eeace0a4c87d3695e899341291720da2
Fabrice <fabrice.douchant@gmail.com>
2018-04-27 21:39:07 +0100

    add newsletter functionality (popup and rss items)

--- squelettes/scripts/main.js
+++ squelettes/scripts/main.js
@@ -9,4 +9,15 @@ jQuery(function ($) {
     // activate textillate
     $('.tlt').textillate();
 
+    // show newsletter modal once and after 5 sec
+    setTimeout(function () {
+        var newsletterCookieName = "newsletterModalShown";
+        if (!Cookies.get(newsletterCookieName)) {
+            if ($("#newsletterModal").length) {
+                $("#newsletterModal").modal('show');
+                Cookies.set(newsletterCookieName, 1, {expires: 365});
+            }
+        }
+    }, 5000);
+
 });
